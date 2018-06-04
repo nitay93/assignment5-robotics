ed5fa8014d6e737dc347aa20fb033c00be1c6974
amir90 <barda1990@gmail.com>
2018-06-04 06:46:35 +0300

    Add files via upload

--- CGAL_defines.h
+++ CGAL_defines.h
@@ -30,10 +30,14 @@ typedef typename Kernel::Vector_2 Vector_2;
 typedef typename Kernel::Line_2 Line_2;
 typedef typename CGAL::Polygon_2<Kernel> Polygon_2;
 typedef typename CGAL::Gps_segment_traits_2< Kernel> Gps_traits_2;
-typedef typename CGAL::Arrangement_2<Gps_traits_2, CGAL::Gps_default_dcel<Gps_traits_2>> Arrangement_2;
+typedef typename CGAL::Arr_extended_dcel<Gps_traits_2,bool,int,bool> Dcel;
+//edge data - if legal wall has an index, otherwise index=-1
+//face data - is obstacle
+typedef typename CGAL::Arrangement_2<Gps_traits_2, Dcel> Arrangement_2;
 typedef typename CGAL::General_polygon_set_2<Gps_traits_2> Polygon_set_2;
 typedef typename Arrangement_2::Vertex_const_handle Arr2_Vertex;
 typedef typename Arrangement_2::Halfedge_const_handle Arr2_hEdge;
+typedef typename Arrangement_2::Face_const_handle Arr2_Face;
 typedef CGAL::Arr_walk_along_line_point_location<Arrangement_2> Walk_pl;
 
 #endif //INC_5_CGAL_DEFINES_H
